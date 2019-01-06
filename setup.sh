0c594f42bf9cc91fcf8fcb0208da81a3e305b028
Scott Chapman <scott@chapman.us>
2019-01-05 20:54:31 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,12 @@ nvm install v8.11.1
 npm install -g node-red pm2 smartthings-mqtt-bridge libbluetooth-dev
 apt install -y mosquitto mosquitto-clients git libbluetooth-dev
 systemctl enable mosquitto.service
+sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
 # launch node-red-pi and then exit
+cd ~/.node-red
+npm install --save node-red-contrib-noble
+pm2 start smartthings-mqtt-bride
+pm2 start node-red-pi
+pm2 save
+pm2 startup
+sudo env PATH=$PATH:/home/pi/.nvm/versions/node/v8.11.1/bin /home/pi/.nvm/versions/node/v8.11.1/lib/node_modules/pm2/bin/pm2 startup systemd -u pi --hp /home/pi
